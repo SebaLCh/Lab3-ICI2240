ba2f2cb6d3541017e0a9adf79342b3c8301bc2cb
Your Name <you@example.com>
2026-04-18 00:04:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -142,7 +142,7 @@ Pair * nextMap(HashMap * map) {
     long pos = map->current+1;
 
     while(map->buckets[pos] == NULL || map->buckets[pos]->key == NULL){
-        if (pos == map->capacity) return NULL;
+        if (pos == map->capacity-1) return NULL;
         pos = (pos+1) % map->capacity;
     }
 
